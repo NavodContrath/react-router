18fa3c8eab108b02e35b9b78f6c685ecd90bfc87
NavodContrath <navod.contrath@gmail.com>
2025-04-01 17:39:45 +0200

    Add Products page to display available items

---
+++ src/pages/Products.jsx
@@ -0,0 +1,49 @@
+import products from "../data/products"
+export default function Products() {
+    return (
+        <>
+            <main className="my-5">
+                <section>
+                    <div className="jumbo mb-5 mt-3 ">
+                        <div className="container bg-dark p-5 border">
+                            <h1>What we offer...</h1>
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla vero atque, voluptatum ducimus delectus laboriosam incidunt voluptas maxime debitis ab hic similique ex mollitia veritatis animi optio aut explicabo molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta in, quam quaerat perferendis, doloribus esse eos culpa saepe eum fugiat doloremque repellat architecto quas, a dolor? Eveniet at dolor cupiditate?
+                            </p>
+                            <button className="btn btn-outline-success" type="button">Go to shop..</button>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div className="container border p-3">
+                        <div className="g-3 row">
+                            {
+                                products.map(prod => {
+                                    return (
+                                        <div className="col-3">
+                                            <div className="card">
+                                                <div className="card-head p-3">
+                                                    <h3>{prod.title}</h3>
+                                                </div>
+                                                <div className="card-body">
+                                                    <img src="" alt="" />
+                                                    <p>{prod.price} $</p>
+                                                </div>
+                                                <div className="card-foot p-3">
+                                                    <p>{prod.description}</p>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                </section>
+            </main>
+
+
+        </>
+    )
+}
