00d2766615390bc1704ea9f742d5ce616b8c60e6
NavodContrath <navod.contrath@gmail.com>
2025-04-01 17:01:45 +0200

    Add About Us page with team descriptions and registration button

---
+++ src/pages/About-us.jsx
@@ -0,0 +1,89 @@
+export default function AboutUs() {
+    return (
+        <>
+            <main className="my-5">
+                <section>
+                    <div className="jumbo mb-5 mt-3 ">
+                        <div className="container bg-dark p-5 border">
+                            <h1>What we do?</h1>
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla vero atque, voluptatum ducimus delectus laboriosam incidunt voluptas maxime debitis ab hic similique ex mollitia veritatis animi optio aut explicabo molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta in, quam quaerat perferendis, doloribus esse eos culpa saepe eum fugiat doloremque repellat architecto quas, a dolor? Eveniet at dolor cupiditate?
+                            </p>
+                            <button className="btn btn-outline-success" type="button">Register now!</button>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div className="description">
+                        <div className="container row g-5  m-auto">
+                            <h1 className="my-3">Our Teams</h1>
+                            <div className="col-4">
+                                <div className="card" >
+                                    <img src="https://picsum.photos/id/29/4000/2670" width={"100%"} alt="" />
+                                </div>
+                            </div>
+                            <div className="col-8">
+                                <div className="card p-3">
+                                    <div className="card-head p-3 border-bottom">
+                                        <h2>Team 1</h2>
+                                    </div>
+                                    <div className="card-body p-2 mt-3">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quas numquam, expedita a beatae aperiam dolore temporibus, nemo quibusdam, asperiores nam iusto dolores doloremque quam obcaecati deleniti nesciunt necessitatibus ipsam!</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-4">
+                                <div className="card" >
+                                    <img src="https://picsum.photos/id/29/4000/2670" width={"100%"} alt="" />
+                                </div>
+                            </div>
+                            <div className="col-8">
+                                <div className="card p-3">
+                                    <div className="card-head p-3 border-bottom">
+                                        <h2>Team 2</h2>
+                                    </div>
+                                    <div className="card-body p-2 mt-3">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quas numquam, expedita a beatae aperiam dolore temporibus, nemo quibusdam, asperiores nam iusto dolores doloremque quam obcaecati deleniti nesciunt necessitatibus ipsam!</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-4">
+                                <div className="card" >
+                                    <img src="https://picsum.photos/id/29/4000/2670" width={"100%"} alt="" />
+                                </div>
+                            </div>
+                            <div className="col-8">
+                                <div className="card p-3">
+                                    <div className="card-head p-3 border-bottom">
+                                        <h2>Team 3</h2>
+                                    </div>
+                                    <div className="card-body p-2 mt-3">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quas numquam, expedita a beatae aperiam dolore temporibus, nemo quibusdam, asperiores nam iusto dolores doloremque quam obcaecati deleniti nesciunt necessitatibus ipsam!</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-4">
+                                <div className="card" >
+                                    <img src="https://picsum.photos/id/29/4000/2670" width={"100%"} alt="" />
+                                </div>
+                            </div>
+                            <div className="col-8">
+                                <div className="card p-3">
+                                    <div className="card-head p-3 border-bottom">
+                                        <h2>Team 4</h2>
+                                    </div>
+                                    <div className="card-body p-2 mt-3">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quas numquam, expedita a beatae aperiam dolore temporibus, nemo quibusdam, asperiores nam iusto dolores doloremque quam obcaecati deleniti nesciunt necessitatibus ipsam!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+
+        </>
+    )
+}
