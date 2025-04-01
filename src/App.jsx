2b6b21aa62a093587c1e1798db48455c82656918
NavodContrath <navod.contrath@gmail.com>
2025-04-01 15:55:14 +0200

    Add header component with navigation links to App

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,27 @@ function App() {
 
   return (
     <>
+      <header className="bg-primary py-3">
+        <div className="container">
+          <div className="d-flex justify-content-between">
+            <div className="logo">
+              <h2>LOGO</h2>
+            </div>
+            <ul className="nav justify-content-between">
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#" aria-current="page">Active link</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">Link</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">Disabled link</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+
     </>
   )
 }
